19e68fa6bf7c7935ebb0683e554505c9d455ba06
shubharrow <shubhamb619@gmail.com>
2023-10-17 15:57:06 +0530

    new

--- Functions.js
+++ Functions.js
@@ -13,9 +13,12 @@ console.log(sum)
 
 let sumofinteger = function(c,d)
     {
+        var greet = "morning"  //the scope of var keyword is insdie this function only it will die outside this function
         return c + d
     }
 
     let sumofNumber = ( c,d )=>c+d  // => this symbol is called fat pipe and use to represent anonymous function.
     //means you dont need to write function keyword here only here (c,d)will work, => is representation of function keyword.
     console.log(sumofNumber(2,3))
+
+    
