9ac93d0c9a0745f278ee7b3c26240d6ee8c478f0
shubharrow <shubhamb619@gmail.com>
2023-10-26 13:46:41 +0530

    26/10/23

--- Strings_and _its_methods.js
+++ Strings_and _its_methods.js
@@ -1,3 +1,7 @@
-let day = "Tuesday" // here day is a string and "" and  '' both can be used to write string 
+let day ="Tuesday" // here day is a string and "" and  '' both can be used to write string 
 console.log(day.length) // length method is used to show length of string
 console.log(day.slice(0,4)) // slice is used to get a substring from string
+console.log(day[4]) // for getting the value of string at particular index
+let splitstring = day.split("s") // split method is used to split the string from a particular word into two words,
+// not includung the word used to split.
+console.log(splitstring)
