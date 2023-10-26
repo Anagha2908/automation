eaf269acfdc0c5e865441c0a9fc8f5ab15749b6e
shubharrow <shubhamb619@gmail.com>
2023-10-26 13:33:58 +0530

    26/10/23

---
+++ Strings_and _its_methods.js
@@ -0,0 +1,3 @@
+let day = "Tuesday" // here day is a string and "" and  '' both can be used to write string 
+console.log(day.length) // length method is used to show length of string
+console.log(day.slice(0,4)) // slice is used to get a substring from string
