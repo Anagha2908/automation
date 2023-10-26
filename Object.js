1dcce11d3d699b9b98eea352faf92c2bbf4feffa
shubharrow <shubhamb619@gmail.com>
2023-10-26 16:40:05 +0530

    26/10/23

--- Object.js
+++ Object.js
@@ -1,13 +1,27 @@
 //object is collection of properties
 let person = {
-firstName: 'Tim' ,
-lastName : 'joe'
+    firstName: 'Tim', //object can include string, integers and even methods
+    lastName: 'joe',
+    age: 24,
+    fullName : function () {
+        console.log(this.firstName + this.lastName) // inside a object you cannot call it property only with help of this keyword you can call.
+    } 
 }
 console.log(person.lastName) //this and below line will give same output
-console.log(person['lastName']) //here lastename is used as index value
+console.log(person['lastName']) //here lastname is used as index value
 
 // to assign any value in object
-person.firstName="shubham"
+person.firstName = "shubham"
 console.log(person.firstName) // if the firstname property already exist so it will go and update the value.
 person.gender = "male"
 console.log(person.gender) // the gender property didnt exist but it got created here.
+delete person.gender // to delete a property from object this delete keyword is used.
+console.log(person.gender) // output will be undefined.
+
+console.log("gender" in person) // to check if gender property is there in person object.
+// output is false
+
+for (let key in person) // enhanced for loop to print ever thing in an object
+{                         // key is actually the properties of object it pick one by one and prints
+    console.log(person[key])
+}
