ebeec4520ed3bed7199b3f678061ae032aa26f3e
shubharrow <shubhamb619@gmail.com>
2023-10-26 15:19:48 +0530

    26/10/23

--- Strings_and _its_methods.js
+++ Strings_and _its_methods.js
@@ -7,3 +7,18 @@ let splitstring = day.split("s") // split method is used to split the string fro
 console.log(splitstring) //now if i want only the right portion of splited string use[0]
 console.log(splitstring[1].length) // for left side of string use [1]
 console.log(splitstring[1].trim().length) //Removes the leading and trailing white space and line terminator characters from a string.
+
+let date ='23' 
+let nextDate = '27' //initials considered as string so for calculation you need to convert it to integer
+let diff = parseInt(nextDate) - parseInt(date) // parseint Converts a string to an integer.
+console.log(diff)
+console.log(diff.toString) // tostring - Returns a string representation of an object.
+
+let newquote = day + "is funday" // + is use to concatinate means to add.
+console.log(newquote)
+
+let val = newquote.indexOf("day") //shows that the day word starts from 4th index , word is tuesday
+console.log(val)
+
+let val1 = newquote.indexOf("day",5) //here there are two "day" in sentence so to select 2nd day index value after 4th is choosen.
+console.log(val1)
