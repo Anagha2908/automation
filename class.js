61c5f4da66e304aab958be5c237ae6865e34dd89
shubharrow <shubhamb619@gmail.com>
2023-10-30 13:32:39 +0530

    30/10/23

---
+++ class.js
@@ -0,0 +1,17 @@
+class Person
+{
+age = 25
+
+ get location() // get defines that its just a property not a method. used for pom
+{
+    return"canada"
+}
+
+// constructor is method which executes by default when you create object of the class 
+
+
+}
+
+let person = new Person()
+console.log(person.age)
+console.log(person.location)
