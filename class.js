92194464dd71ddab826233f4f6f7ec6ac0d453bf
shubharrow <shubhamb619@gmail.com>
2023-10-31 11:26:25 +0530

    31/10/23

--- class.js
+++ class.js
@@ -1,17 +1,27 @@
-class Person
-{
-age = 25
+class Person {
+    age = 25
 
- get location() // get defines that its just a property not a method. used for pom
-{
-    return"canada"
+    get location() // get defines that its just a property not a method. used for pom
+    {
+        return "canada"
+    }
+
+    // constructor is method which executes by default when you create object of the class 
+    constructor(firstname, lastname) {
+        this.firstname = firstname
+        this.lastname = lastname
+    }
+    //method
+    fullname1()
+    {
+        console.log(this.firstname+this.lastname)
+    }
+
 }
 
-// constructor is method which executes by default when you create object of the class 
-
-
-}
-
-let person = new Person()
+let person = new Person("shubh","arrow")
+let person1 = new Person("shubh","sharma")
 console.log(person.age)
 console.log(person.location)
+console.log(person.fullname1())
+console.log(person1.fullname1())
