82d6def31adbb14106ac4b12bf240ec97935dabb
shubharrow <96536869+shubharrow@users.noreply.github.com>
2024-02-28 16:10:09 +0530

    29/02/24

---
+++ PlayWright/Playwright.js
@@ -0,0 +1,3 @@
+// npm init playwright // to install playwright to a particular folder use :CD to navigate
+// package.json contains all the dependencies
+// node modules include all the installed jars
